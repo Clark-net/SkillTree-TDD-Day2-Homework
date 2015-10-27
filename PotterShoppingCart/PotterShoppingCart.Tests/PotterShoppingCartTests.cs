a6dd869bcd257049ce695e79c2589bb36f088011
Clark-net <aquariuscamos@hotmail.com>
2015-10-28 01:43:27 +0800

    1. 建立測試案例：一二三四集各買了一本_價格應為320元。 2. 測試失敗：預期320，實際400。

--- PotterShoppingCart/PotterShoppingCart.Tests/PotterShoppingCartTests.cs
+++ PotterShoppingCart/PotterShoppingCart.Tests/PotterShoppingCartTests.cs
@@ -136,5 +136,33 @@ namespace PotterShoppingCart.Tests
             //assert
             Assert.AreEqual(expected, target.TotalAmount);
         }
+        
+        [TestMethod]
+        public void TestShoppingCart_一二三四集各買了一本_價格應為320元()
+        {
+            //Scenario: 一二三四集各買了一本，價格應為100*4*0.8=320
+            //    Given 第一集買了 1 本
+            //    And 第二集買了 1 本
+            //    And 第三集買了 1 本
+            //    And 第四集買了 1 本
+            //    And 第五集買了 0 本
+            //    When 結帳
+            //    Then 價格應為 320 元
+
+            //arrange
+            var target = new ShoppingCart();
+            target.AddBooks(new Book { Name = "哈利波特第一集" });
+            target.AddBooks(new Book { Name = "哈利波特第二集" });
+            target.AddBooks(new Book { Name = "哈利波特第三集" });
+            target.AddBooks(new Book { Name = "哈利波特第四集" });
+
+            var expected = 320;
+
+            //act
+            target.Checkout();
+
+            //assert
+            Assert.AreEqual(expected, target.TotalAmount);
+        }
     }
 }
